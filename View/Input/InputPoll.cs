1ac07dbd90c4efdfba2c85c667255256124eb62b
Cuku <xhikab@gmail.com>
2026-08-22 17:34:05 +0200

    Re-structure code and assets inside QuantumUser

---
+++ View/Input/InputPoll.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using Photon.Deterministic;
+using Quantum;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputPoll : MonoBehaviour {
+	[SerializeField] private InputActionReference moveAction;
+	[SerializeField] private InputActionReference lookAction;
+	[SerializeField] private InputActionReference jumpAction;
+	[SerializeField] private InputActionReference sprintAction;
+	[SerializeField] private InputActionReference leftClawKickAction;
+	[SerializeField] private InputActionReference rightClawKickAction;
+	[SerializeField] private InputActionReference leftClawReachAction;
+	[SerializeField] private InputActionReference rightClawReachAction;
+	[SerializeField] private InputActionReference leftClawPinchAction;
+	[SerializeField] private InputActionReference rightClawPinchAction;
+
+	private readonly Dictionary<PlayerInput, CachedActions> actionCache = new();
+
+	private class CachedActions {
+		public InputAction Move;
+		public InputAction Look;
+		public InputAction Jump;
+		public InputAction Sprint;
+		public InputAction LeftClawKick;
+		public InputAction RightClawKick;
+		public InputAction LeftClawReach;
+		public InputAction RightClawReach;
+		public InputAction LeftClawPinch;
+		public InputAction RightClawPinch;
+	}
+
+	private void Start() {
+		QuantumCallback.Subscribe<CallbackPollInput>(this, PollInput);
+	}
+
+	private void PollInput(CallbackPollInput callback) {
+		PlayerInputData input = default;
+
+		var localPlayers = callback.Game.GetLocalPlayers();
+		if (localPlayers.Count == 0)
+			return;
+
+		var player = localPlayers[callback.PlayerSlot];
+		LocalPlayerAccess localPlayerAccess = LocalPlayersController.Instance.GetLocalPlayerAccess(player);
+
+		if (localPlayerAccess != null && localPlayerAccess.LocalPlayer != null) {
+			PlayerInput playerInput = localPlayerAccess.PlayerInput;
+
+			if (!actionCache.TryGetValue(playerInput, out CachedActions actions)) {
+				actions = new CachedActions {
+					Move = playerInput.actions.FindAction(moveAction.action.id),
+					Look = playerInput.actions.FindAction(lookAction.action.id),
+					Jump = playerInput.actions.FindAction(jumpAction.action.id),
+					Sprint = playerInput.actions.FindAction(sprintAction.action.id),
+					LeftClawReach = playerInput.actions.FindAction(leftClawReachAction.action.id),
+					RightClawReach = playerInput.actions.FindAction(rightClawReachAction.action.id),
+					LeftClawKick = playerInput.actions.FindAction(leftClawKickAction.action.id),
+					RightClawKick = playerInput.actions.FindAction(rightClawKickAction.action.id),
+					LeftClawPinch = playerInput.actions.FindAction(leftClawPinchAction.action.id),
+					RightClawPinch = playerInput.actions.FindAction(rightClawPinchAction.action.id)
+				};
+
+				actionCache.Add(playerInput, actions);
+			}
+
+			input.MoveDirection = actions.Move.ReadValue<Vector2>().ToFPVector2();
+
+			Vector2 look = actions.Look.ReadValue<Vector2>();
+			input.AimDirection = new Vector2(look.x, 0).ToFPVector2();
+
+			input.Jump = actions.Jump.IsPressed();
+			input.Sprint = actions.Sprint.IsPressed();
+			input.LeftClawReach = FP.FromFloat_UNSAFE(actions.LeftClawReach.ReadValue<float>());
+			input.RightClawReach = FP.FromFloat_UNSAFE(actions.RightClawReach.ReadValue<float>());
+			input.LeftClawKick = actions.LeftClawKick.IsPressed();
+			input.RightClawKick = actions.RightClawKick.IsPressed();
+			input.LeftClawPinch = actions.LeftClawPinch.IsPressed();
+			input.RightClawPinch = actions.RightClawPinch.IsPressed();
+		}
+
+		callback.SetInput(input, DeterministicInputFlags.Repeatable);
+	}
+}
